cd59ec9c54b843907574e2d3926420dc030077a9
Richard Rocha <richibaby0801@hotmail.com>
2023-09-21 13:10:32 -0500

    Avance1
    salio un error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
+import {signOut, getSession} from 'next-auth/react';
+import { NextPageContext } from 'next';
+
+export async function getServerSideProps(context:NextPageContext) {
+  const session = await getSession(context);
+
+  if(!session){
+    return{
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return{
+    props: {}
+  }
+
+
+}
+
+
+
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl text-green-500">DATA SALMON</h1>
+      <h1 className="text-4xl text-green-500">DATA SALMON inDEX</h1>
+      <button
+      className='h-10 w-full bg-white'
+        onClick={() => signOut()}
+      >Logout!</button>
+    
     </>
   );
 }
